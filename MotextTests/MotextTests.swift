e4e698d5624548c3feb1a7a88010c19dfbb7490e
Zachary Stegall <zachstegall@gmail.com>
2019-01-11 00:46:52 -0800

    Initial Commit

---
+++ MotextTests/MotextTests.swift
@@ -0,0 +1,34 @@
+//
+//  MotextTests.swift
+//  MotextTests
+//
+//  Created by Zachary Stegall on 1/11/19.
+//  Copyright © 2019 Zachary Stegall. All rights reserved.
+//
+
+import XCTest
+@testable import Motext
+
+class MotextTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
